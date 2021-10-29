dae87aace676941fe43ecb82cd443151d20a77a4
Hanuma G <ghanum@sutherlandglobal.com>
2021-10-29 13:14:44 +0530

    10/29

--- constants.js
+++ constants.js
@@ -25,7 +25,7 @@ const prompts = [
   ["no","not sure","maybe","no thanks"],
   [""],
   ["haha","ha","lol","hehe","funny","joke"],
-  ["SWA"]
+  ["SMART"]
 ]
 
 // Possible responses, in corresponding order
